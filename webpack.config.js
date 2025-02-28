e6c0b00eb35bd43fb7ada2c92a5cc51b06d98e63
DMArt1984 <dmxart13@gmail.com>
2025-02-28 16:40:45 +0500

    Добавьте файлы проекта.

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+﻿const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './wwwroot/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './Views/Home/Index.cshtml',
+            filename: 'index.html',
+        }),
+    ],
+    devServer: {
+        static: path.resolve(__dirname, 'wwwroot/dist'),
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    mode: 'development',
+};
